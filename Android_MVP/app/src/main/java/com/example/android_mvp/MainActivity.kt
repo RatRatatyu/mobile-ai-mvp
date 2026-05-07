c508b2a89ac0b959d51ee8ce6e93c3bb4dd1093c
Margarita <o.komarinets@gmail.com>
2026-05-07 20:23:04 +0500

    refactor: move MainScreen to a separate file and clean up MainActivity imports

--- Android_MVP/app/src/main/java/com/example/android_mvp/MainActivity.kt
+++ Android_MVP/app/src/main/java/com/example/android_mvp/MainActivity.kt
@@ -1,37 +1,16 @@
 package com.example.android_mvp
 
-import android.Manifest
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.android_mvp.core.MainContract
 import com.example.android_mvp.core.MainPresenter
+import com.example.android_mvp.data.MainScreen
 import com.example.android_mvp.ui.theme.Android_MVPTheme
-
 
 
 class MainActivity : ComponentActivity(), MainContract.View {
@@ -60,38 +39,5 @@ class MainActivity : ComponentActivity(), MainContract.View {
         hasCameraPermission = state
     }
 
-
-
-
 }
 
-
-@Composable
-fun MainScreen(
-    hasPermission: Boolean,
-    onRequestCameraClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Scaffold(
-        modifier = modifier.fillMaxSize()
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-
-        ) {
-            Box (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f)
-            ){
-                if(hasPermission){
-                    Text("hi", textAlign = TextAlign.Center)
-                }else{
-                    Button(onClick = onRequestCameraClick) {Text("Premmisom") }
-                }
-            }
-        }
-    }
-}
