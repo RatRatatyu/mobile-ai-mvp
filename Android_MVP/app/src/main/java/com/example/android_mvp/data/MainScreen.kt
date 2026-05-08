9c4134f51ddc56975f733c1ecb1ec4f278a22ddc
Margarita <o.komarinets@gmail.com>
2026-05-08 12:08:39 +0500

    feat: update MainScreen to capture and display photos using ImageProxy and Bitmaps

--- Android_MVP/app/src/main/java/com/example/android_mvp/data/MainScreen.kt
+++ Android_MVP/app/src/main/java/com/example/android_mvp/data/MainScreen.kt
@@ -1,19 +1,17 @@
 package com.example.android_mvp.data
 
-import android.net.Uri
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,19 +20,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import coil.compose.rememberAsyncImagePainter
 import com.example.android_mvp.R
 import com.example.android_mvp.data.camera.CameraPreview
 import com.example.android_mvp.data.camera.takePhoto
-import com.example.android_mvp.ui.theme.Android_MVPTheme
+import com.example.android_mvp.data.camera.toCorrectlyRotatedBitmap
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,7 +42,7 @@ fun MainScreen(
     onRequestCameraClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val isImageTaken by remember { mutableStateOf<Uri?>(null) }
+    var latestPhoto by remember { mutableStateOf<ImageProxy?>(null) }
 
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
@@ -51,6 +50,7 @@ fun MainScreen(
         LifecycleCameraController(context).apply {
             cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             bindToLifecycle(lifecycleOwner)
+            setEnabledUseCases(CameraController.IMAGE_CAPTURE)
         }
     }
 
@@ -62,7 +62,8 @@ fun MainScreen(
                 title = {
                     Text(
                         stringResource(R.string.app_name),
-                        style = MaterialTheme.typography.titleLarge)
+                        style = MaterialTheme.typography.titleLarge
+                    )
                 }
             )
         }
@@ -73,61 +74,61 @@ fun MainScreen(
                 .fillMaxSize()
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+        ) {
 
-            Box (
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(all = 20.dp)
                     .weight(2f)
                     .clip(RoundedCornerShape(12.dp)),
                 contentAlignment = Alignment.Center
-            ){
-                if(hasPermission){
-                    CameraPreview(
-                        context, lifecycleOwner , cameraController, modifier
-                    )
-                }else{
-                    Button(onClick = onRequestCameraClick) {Text(stringResource(R.string.ask_permission_button)) }
+            ) {
+                if (hasPermission) {
+                    if(latestPhoto == null) {
+                        CameraPreview(
+                            cameraController, modifier
+                        )
+                    }else{
+                        latestPhoto?.let { imageProxy ->
+                            val bitmap = imageProxy.toCorrectlyRotatedBitmap()
+
+                            Image(
+                                bitmap = bitmap.asImageBitmap(),
+                                contentDescription = "Сделанное фото",
+                                modifier = Modifier.fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+
+                        }
+                    }
+                } else {
+                    Button(onClick = onRequestCameraClick) { Text(stringResource(R.string.ask_permission_button)) }
                 }
             }
-            Box (
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(all = 20.dp)
                     .weight(2f),
                 contentAlignment = Alignment.Center
-            ){
-                Button(onClick = { takePhoto(cameraController, context, {  }) }) {
-                    Text("hi")
+            ) {
+                Button(
+                    onClick = {
+                        takePhoto(cameraController, context) { photo ->
+                            latestPhoto = photo
+                        }
+                    },
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text("📸 Сфоткать")
                 }
-                isImageTaken?.let { uri ->
-                    Box(modifier = Modifier.padding(16.dp)) {
-                        Image(
-                            painter = rememberAsyncImagePainter(uri),
-                            contentDescription = "Сделанное фото",
-                            modifier = Modifier
-                                .size(200.dp)
-                                .clip(RoundedCornerShape(12.dp))
-                        )
-                    }
 
-                }
+
 
             }
-
-
         }
     }
 }
 
-@Preview
-@Composable
-private fun PreviewMainScree() {
-    Android_MVPTheme{
-        MainScreen(
-            false,
-            {print("hi")}
-        )
-    }
-}
+
