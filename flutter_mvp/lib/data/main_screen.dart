a854ed0dd1b19ed246f5fa79737b5ea2fe209d48
Margarita <o.komarinets@gmail.com>
2026-05-12 19:48:56 +0500

    feat: implement classification result display and loading state in MyHomePage

--- flutter_mvp/lib/data/main_screen.dart
+++ flutter_mvp/lib/data/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvp/core/ui_state_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../core/provider_menadger.dart';
@@ -10,7 +11,7 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
-
+    final uiState = context.watch<UiStateProvider>();
 
     return Scaffold(
       appBar: AppBar(
@@ -20,11 +21,29 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ShowingCamera(),
 
           Expanded(
-              child: Placeholder()
+              child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(15),
+                  child: uiState.isLoading
+                      ? const Center(child: CircularProgressIndicator(),)
+                      : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Объект: ${uiState.label}",
+                        style: Theme.of(context).textTheme.headlineSmall,),
+                      Text("Увереность: ${uiState.score}%",
+                        style: Theme.of(context).textTheme.headlineSmall,),
+                      Text("Время: 0",
+                        style: Theme.of(context).textTheme.headlineSmall,),
+
+                    ],
+                  )
+              )
           )
         ],
       ),
