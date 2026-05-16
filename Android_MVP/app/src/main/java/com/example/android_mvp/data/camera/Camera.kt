51ebea3341b6f52fe835d03e478da1299398a033
Margarita <o.komarinets@gmail.com>
2026-05-17 00:37:59 +0500

    refactor: replace CameraHandler with CameraHelper composable and implement integrated photo preview logic

--- Android_MVP/app/src/main/java/com/example/android_mvp/data/camera/Camera.kt
+++ Android_MVP/app/src/main/java/com/example/android_mvp/data/camera/Camera.kt
@@ -1,6 +1,5 @@
 package com.example.android_mvp.data.camera
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
@@ -14,85 +13,42 @@ import androidx.camera.core.ImageProxy
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.RestartAlt
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.android_mvp.R
-import com.example.android_mvp.data.model.ApiModel
-import com.example.android_mvp.data.model.MlKitImageLabeler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-
-
-
-class CameraHandler{
-
-    fun takePhoto(
-        cameraController: LifecycleCameraController,
-        context: Context,
-        onClassificationResult: (String, Float, Long) -> Unit,
-        isOnDevice: Boolean,
-        scope: CoroutineScope,
-        onPhotoTaken: (Bitmap?) -> Unit     //  returning ImageProxy
-    ) {
-        val executor = ContextCompat.getMainExecutor(context)
-
-        cameraController.takePicture(
-            executor,
-            object : ImageCapture.OnImageCapturedCallback() {
-                @OptIn(ExperimentalGetImage::class)
-                override fun onCaptureSuccess(image: ImageProxy) {
-
-                    val bitmap = image.toCorrectlyRotatedBitmap()
-                    onPhotoTaken(bitmap)
-                    image.close()
-
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    Log.e("Camera", context.getString(R.string.error_photo), exception)
-                    onPhotoTaken(null)
-                }
-            }
-        )
-    }
-
-    fun ImageProxy.toCorrectlyRotatedBitmap(): Bitmap {
-        val buffer = planes[0].buffer
-        val bytes = ByteArray(buffer.remaining())
-        buffer.get(bytes)
-
-        val originalBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-
-        val matrix = Matrix().apply {
-            postRotate(imageInfo.rotationDegrees.toFloat())
-        }
-
-        return Bitmap.createBitmap(
-            originalBitmap,
-            0,
-            0,
-            originalBitmap.width,
-            originalBitmap.height,
-            matrix,
-            true
-        )
-    }
-}
-
-
-
-
-
 
 @Composable
-fun CameraPreview(
+fun CameraHelper(
+    latestPhoto: Bitmap?,
+    onTakePhoto: (Bitmap?) -> Unit,
+    onCleanPhoto: () -> Unit,
     modifier: Modifier
 ) {
     val context = LocalContext.current
@@ -103,19 +59,133 @@ fun CameraPreview(
             setEnabledUseCases(CameraController.IMAGE_CAPTURE)
         }
     }
-    AndroidView(
-        modifier = modifier.fillMaxSize(),
-        factory = {ctx->
-            PreviewView(ctx).apply {
-                this.controller = cameraController
+
+    fun onTakePhoto(
+    ) {
+        val executor = ContextCompat.getMainExecutor(context)
+
+        cameraController.takePicture(
+            executor,
+            object : ImageCapture.OnImageCapturedCallback() {
+                @OptIn(ExperimentalGetImage::class)
+                override fun onCaptureSuccess(image: ImageProxy) {
+
+                    val bitmap = image.toCorrectlyRotatedBitmap()
+                    onTakePhoto(bitmap)
+                    image.close()
+
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e("Camera", "camera error", exception)
+                    onTakePhoto(null)
+
+                }
             }
-        },
-        update = {
-            cameraController.bindToLifecycle(lifecycleOwner)
+        )
+    }
+
+    Column(modifier = modifier) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 20.dp)
+                .weight(3f)
+                .clip(RoundedCornerShape(12.dp)),
+            contentAlignment = Alignment.Center
+        ){
+            if(latestPhoto == null){
+                AndroidView(
+                    modifier = modifier.fillMaxSize(),
+                    factory = {ctx->
+                        PreviewView(ctx).apply {
+                            this.controller = cameraController
+                        }
+                    },
+                    update = {
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                )
+            }else{
+                Image(
+                    bitmap = latestPhoto.asImageBitmap(),
+                    contentDescription = stringResource(R.string.photo_description),
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
         }
-    )
+
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            if(latestPhoto == null){
+                IconButtonHelper(
+                    { onTakePhoto },
+                    Icons.Filled.PhotoCamera,
+                    "Сделать фото"
+                )
+            }else{
+                IconButtonHelper(
+                    { onCleanPhoto },
+                    Icons.Filled.RestartAlt,
+                    "Сфоткать снова"
+                )
+            }
+
+        }
+
+    }
 }
 
+@Composable
+fun IconButtonHelper(
+    onClickFun: () -> Unit,
+    icon: ImageVector,
+    contectDescriptionText: String,
+) {
 
+    IconButton(
+        onClick = onClickFun,
+        modifier = Modifier
+            .size(72.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = contectDescriptionText,
+            modifier = Modifier.size(42.dp),
+            tint = MaterialTheme.colorScheme.inversePrimary
+        )
+    }
+
+}
+
+fun ImageProxy.toCorrectlyRotatedBitmap(): Bitmap {
+    val buffer = planes[0].buffer
+    val bytes = ByteArray(buffer.remaining())
+    buffer.get(bytes)
+
+    val originalBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+    val matrix = Matrix().apply {
+        postRotate(imageInfo.rotationDegrees.toFloat())
+    }
+
+    return Bitmap.createBitmap(
+        originalBitmap,
+        0,
+        0,
+        originalBitmap.width,
+        originalBitmap.height,
+        matrix,
+        true
+    )
+}
 
 
