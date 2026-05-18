40cf2503c3554b98789786065575d94dcc7437da
Margarita <o.komarinets@gmail.com>
2026-05-19 00:02:11 +0500

    style: remove unused Compose runtime imports in MainScreen.kt

--- Android_MVP/app/src/main/java/com/example/android_mvp/data/MainScreen.kt
+++ Android_MVP/app/src/main/java/com/example/android_mvp/data/MainScreen.kt
@@ -16,11 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
